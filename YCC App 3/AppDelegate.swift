0888c4bb3feee412917e3f58d9a64b7673a7cb0d
Vikram <dr.vikramraj87@gmail.com>
2018-11-17 07:00:16 +0530

    Initial Commit

---
+++ YCC App 3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YCC App 3
+//
+//  Created by Vikram Raj Gopinathan on 17/11/18.
+//  Copyright © 2018 Vikram Raj Gopinathan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YCC App 3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YCC App 3
+//
+//  Created by Vikram Raj Gopinathan on 17/11/18.
+//  Copyright © 2018 Vikram Raj Gopinathan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
